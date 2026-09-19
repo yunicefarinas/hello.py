590a826641bf2fc36bf417adf3702e5081263654
Yunice <farinasyuniceangel@gmail.com>
2026-09-19 08:14:17 -0400

    Delete electric_charges.py

--- electric_charges.py
+++
@@ -1,8 +0,0 @@
-kwHours = int(input("Enter the KW hours used: "))
-
-if kwHours <= 1000:
-    amount = kwHours * 7.633 / 100
-else:
-    amount = (1000 * 7.633 / 100) + ((kwHours - 1000) * 9.259 / 100)
-
-print("Amount owed is $", amount)
